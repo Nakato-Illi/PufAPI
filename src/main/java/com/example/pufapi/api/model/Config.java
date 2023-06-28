eb5053e48abe7816f0074ce41059e290790643b3
amani2409 <einkauf-a@posteo.de>
2023-06-28 21:24:07 +0200

    clean Code

--- src/main/java/com/example/pufapi/api/model/Config.java
+++ src/main/java/com/example/pufapi/api/model/Config.java
@@ -5,6 +5,6 @@ public class Config {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPassword = "******";
+    protected String dbPassword = "*******";
     protected String dbName = "login";
 }
